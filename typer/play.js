533179ea442285f75c29356dc355b7b303c25e6b
wareesha-nasir <57236937+wareesha-nasir@users.noreply.github.com>
2020-04-06 12:07:07 -0700

    Add files via upload

--- typer/play.js
+++ typer/play.js
@@ -1,41 +1,99 @@
-let timer=5;
-let score=0;
-let playing;
+window.addEventListener('load',newtext)
+let timer=15
+let score=0
+let playing
 
-const ranword= "http://api.quotable.io/random";
+const level=document.getElementById("option")
 const display=document.getElementById("text")
 const input=document.getElementById("input")
 const scores=document.querySelector("#score")
 const time=document.querySelector("#timer")
 const alerrt=document.querySelector("#message")
-function getrandom()
-{
-  return fetch(ranword)
-  .then(response=>response.json())
-  .then(data=>data.content)
-}
-async function newtext(){
-  const quote=await getrandom()
-  display.innerText=quote
-  input.value=null
+const words=[
+'happy',
+  'superficial',
+  'dove',
+  'bruise',
+  'horrible',
+  'desperate',
+  'temperature',
+  'flock',
+  'stagnant',
+  'awful',
+  'amazing',
+  'sacrifice',
+  'practice',
+  'cheerful',
+  'lubricants',
+  'knight',
+  'igloo',
+  'jeopardy',
+  'entertainment',
+  'zeal',
+  'yatch',
+  'glorious',
+  'fundamental',
+  'nomads',
+  'miserable',
+  'terrified',
+'robotics',
+'wonderful',
+'quince'
+];
+
+ function newtext(){
+   //display words
+  showWord(words)
+  input.addEventListener('input',matchwords)
   //console.log(quote)
-setInterval(updatetime,1000);
-setInterval(status,50);
+setInterval(updatetime,1000)
+setInterval(status,50)
+}
+function showWord(words){
+  const ranw=Math.floor(Math.random()*words.length)
+  display.innerHTML=words[ranw]
 }
 newtext()
+showWord(words)
 function updatetime()
 {
   if(timer>0){
-    timer--;
+    timer--
   }
   else if(timer===0){
-playing=false;
+playing=false
   }
-  time.innerHTML=timer;
+  time.innerHTML=timer
 }
 function status(){
   if (!playing && timer===0){
-    alerrt.innerHTML="Game is over!!!";
+    alerrt.innerHTML="Game is over!!!"
+    score=-1
   }
 }
+function matchwords(){
+  if(match()){
+    playing=true
+    timer=15
+showWord(words)
+input.value=""
+    score++;
+  }
+  if(score===-1){
+    scores.innerHTML=0
+  }else{
+  scores.innerHTML=score
+  }
+  
+}
+function match(){
+  if(input.value===display.innerHTML){
+    alerrt.innerHTML="correct!!"
+    return true;
+  }else {
+    alerrt.innerHTML=""
+    return false
+  }
+}
+
 
